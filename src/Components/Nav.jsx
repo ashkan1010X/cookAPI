7bc4b87e6b2dd136797b4e2efa23b59491e12fd4
ashkan1010X <aasadpour@myseneca.ca>
2024-10-15 02:10:09 -0400

    nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,7 @@
 import styles from "./nav.module.css";
 
 export default function Nav() {
-  return <div className={styles.nav}>🍔FoodApp</div>;
+  return (
+    <div className={styles.nav}>🥘🥙🥞🍕🍖🍝🍰🍔FoodApp🍔🍰🍝🍖🍕🥞🥙🥘</div>
+  );
 }

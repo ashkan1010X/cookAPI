847feeee1afa7da2ff614c8318e6e52a96c86359
ashkan1010X <aasadpour@myseneca.ca>
2024-10-20 04:39:29 -0400

    nav update

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,9 +3,12 @@ import styles from "./nav.module.css";
 export default function Nav() {
   const emojis = "🥘🥙🥞🍕🍖🍝🍰🍔";
   const emojisS = "🍔🍰🍝🍖🍕🥞🥙🥘";
+
   return (
     <div className={styles.nav}>
-      {emojis} DataDish {emojisS}
+      <div className={styles.emojis}>{emojis}</div>
+      <div className={styles.title}>DataDish</div>
+      <div className={styles.emojis}>{emojisS}</div>
     </div>
   );
 }

61312ff13b3b680ec13535ce255318da0f47d506
ashkan1010X <aasadpour@myseneca.ca>
2024-10-21 02:52:17 -0400

    foodItem condition update

--- src/Components/FoodItem.jsx
+++ src/Components/FoodItem.jsx
@@ -1,6 +1,15 @@
 import styles from "./fooditem.module.css";
 
 export default function FoodItem({ food, setFoodID }) {
+  const handleClick = () => {
+    setFoodID(food.id); // Set the new food ID
+
+    // Check if the viewport width is less than or equal to 768px (mobile)
+    if (window.innerWidth <= 768) {
+      window.scrollTo(0, 0); // Scroll to the top of the page
+    }
+  };
+
   return (
     <div className={styles.itemContainer}>
       <img className={styles.itemImage} src={food.image} alt="" />
@@ -8,10 +17,7 @@ export default function FoodItem({ food, setFoodID }) {
         <p className={styles.itemName}>{food.title}</p>
       </div>
       <div className={styles.buttonContainer}>
-        <button
-          onClick={() => setFoodID(food.id)}
-          className={styles.itemButton}
-        >
+        <button onClick={handleClick} className={styles.itemButton}>
           View Recipe
         </button>
       </div>

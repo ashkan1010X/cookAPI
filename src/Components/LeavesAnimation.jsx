b9c3bdbdfaaf5930f01d7abe6f0606632eab2cb2
ashkan1010X <aasadpour@myseneca.ca>
2024-10-22 03:52:03 -0400

    optimize theme

--- src/Components/LeavesAnimation.jsx
+++ src/Components/LeavesAnimation.jsx
@@ -2,18 +2,31 @@ import React, { useEffect } from "react";
 import styles from "./leavesanimation.module.css";
 
 export default function LeavesAnimation() {
-  const leaves = Array(10).fill(0); // Array to render multiple leaves
+  const leaves = Array(10).fill(0); // Array to render multiple leaves 🍁
+  const halloween = Array(3).fill(0); // Array to render multiple monsters 👻
 
   return (
-    <div className={styles.leavesContainer}>
-      {leaves.map((_, index) => (
-        <img
-          src={`leaves/leaf${index + 1}.png`}
-          key={index}
-          alt="leaf"
-          className={`${styles.leaf} ${styles[`leaf${index + 1}`]}`}
-        />
-      ))}
+    <div>
+      <div className={styles.leavesContainer}>
+        {leaves.map((_, index) => (
+          <img
+            src={`leaves/leaf${index + 1}.png`}
+            key={index}
+            alt="leaf"
+            className={`${styles.leaf} ${styles[`leaf${index + 1}`]}`}
+          />
+        ))}
+      </div>
+      <div className={styles.monstersContainer}>
+        {halloween.map((_, index) => (
+          <img
+            src={`monsters/monster${index + 1}.png`}
+            key={index}
+            alt="monster"
+            className={`${styles.monster} ${styles[`monster${index + 1}`]}`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
